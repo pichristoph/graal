f890cfd46090185b2118d1efedfd9a4db5a578e9
Christoph Pichler <christoph.pichler@jku.at>
2020-08-25 08:33:19 +0200

    fix method typo

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/access/LLVMInteropType.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/access/LLVMInteropType.java
@@ -481,7 +481,7 @@ public abstract class LLVMInteropType implements TruffleObject {
 
         @ExportMessage
         boolean isExecutable() {
-            return InteropLibrary.getUncached(foreignMethodObject).isException(foreignMethodObject);
+            return InteropLibrary.getUncached(foreignMethodObject).isExecutable(foreignMethodObject);
         }
 
         @ExportMessage
